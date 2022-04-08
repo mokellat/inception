2dc888821b531d637c30f60a1fc9bd5179e0a709
mokellat <kellatis55@gmail.com>
2022-04-08 22:11:31 +0700

    still have to fix ports

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 # docker rm -vf $(docker ps -aq)
 # docker rmi wordpress nginx mariadb
 # docker volume rm srcs_db srcs_wordpress
+# sudo rm -rf /home/mokellat/
 run:
 # docker system prune -a
-	sudo rm -rf /home/mokellat/
 	sudo mkdir -p /home/mokellat/data
 	sudo mkdir -p /home/mokellat/data/wp-files
 	sudo mkdir -p /home/mokellat/data/wp-db
 	docker-compose -f srcs/docker-compose.yml up --build
-down:
-	docker-compose -f srcs/docker-compose.yml down
+# down:
+# 	docker-compose -f srcs/docker-compose.yml down
--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -1,10 +1,15 @@
 #!bin/sh
-
-service mysql start
-mysql -e "create database $USERNAME"
-mysql -uroot -proot -e "GRANT ALL ON *.* TO '$USERNAME'@'%' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION"
-mysql mokellat --user=$USERNAME  --password=$PASSWORD < ./conf/wordpress.sql
-mysql -uroot -proot -e "FLUSH PRIVILEGES"
-# tail -f /dev/null
-service mysql stop
-mysqld_safe
+if [ ! -d /var/lib/mysql/mokellat/]
+then
+	service mysql start
+	mysql -e "create database $USERNAME"
+	mysql -uroot -proot -e "GRANT ALL ON *.* TO '$USERNAME'@'%' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION"
+	mysql mokellat --user=$USERNAME  --password=$PASSWORD < ./conf/wordpress.sql
+	mysql -uroot -proot -e "FLUSH PRIVILEGES"
+	# tail -f /dev/null
+	service mysql stop
+	mysqld_safe
+else
+	mysql mokellat --user=$USERNAME  --password=$PASSWORD < ./conf/wordpress.sql
+	mysqld_safe --skip-grant-table
+fi
